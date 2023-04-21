f96336d6c2a2b988927e14386ee2825fbcc74dc1
Yana G <ivana-1@mail.ru>
2023-04-21 20:33:10 +0300

    first commit

---
+++ src/main/java/ru/netology/services/RestService.java
@@ -0,0 +1,21 @@
+package ru.netology.services;
+
+public class RestService {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0;
+        int money = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                count++;
+                money = money - expenses;
+                money = money / 3;
+            } else {
+                money = money + income;
+                money = money - expenses;
+            }
+        }
+        return count;
+
+    }
+}
---
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class RestServiceTest {
+    @Test
+    public void testCalculateQuantity3MonthsRest() {
+        RestService service = new RestService();
+
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCalculateQuantity2MonthsRest() {
+        RestService service = new RestService();
+
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    public void testCalculateQuantityMonthsRest(int income, int expenses, int threshold, int expected) {
+        RestService service = new RestService();
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
